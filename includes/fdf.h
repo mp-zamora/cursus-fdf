e8719bdcca2253953639f0961c126191495a64d3
Miguel Penas Zamora <mpenas-z@c1r7s4.42madrid.com>
2024-09-03 15:23:14 +0200

    Start parse_maap function

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:31:22 by mpenas-z          #+#    #+#             */
-/*   Updated: 2024/09/03 14:31:58 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2024/09/03 15:22:25 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 # define FDF_H
 # include "../libft/libft.h"
 # include <stdlib.h>
+# include <stdio.h>
 # include <limits.h>
 # include <math.h>
+
+typedef	struct	s_point
+{
+	int	x;
+	int	z;
+	int	y;
+	t_point	*next;
+	t_point	*prev;
+}	t_point;
+
+t_point	*parse_map(int argc, char *argv[]);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,41 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:27:02 by mpenas-z          #+#    #+#             */
-/*   Updated: 2024/09/03 14:49:54 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2024/09/03 15:21:48 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
+/* I'll use lists to store the coordinates of every spatial point */
+t_point	*parse_map(int argc, char *argv[])
+{
+	t_point	*points;
+	char	*buffer;
+	int		total_points;
+
+	// Check if args are OK
+	if (argc != 2)
+		return (0);
+	total_points = 0;
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		close (fd);
+		return (0);
+	}
+	/* Fill the list with points */
+	buffer = get_next_line(fd);
+	while (buffer != NULL)
+	{
+		// ft_split buffer, create new node for each element.
+		// Do function that reads string, and creates struct.
+		buffer = get_next_line(fd);
+	}
+	close(fd);
+	return (1);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -20,6 +50,7 @@ int	main(int argc, char *argv[])
 	//	1. Open file given in args.
 	//	2. get_next_line and ft_split result.
 	//	3. For every value, record in a lineal array its x, y and z coordinates.
+	parse_map(argc, argv);
 	/* Then we run MLX and create a graphical representation of said array. */
 	ft_putstr_fd("This is a test\n", 1);
 	/* Finally we free everything when the user exits the GUI. */
